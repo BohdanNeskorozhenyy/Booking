6920b5e77dfe3a55571f6cb64cbc3819ff962f7b
Pasha <pavel@waff.ly>
2022-05-04 10:25:55 +0300

    change button

--- booking/components/VisualPicker.js
+++ booking/components/VisualPicker.js
@@ -1,9 +1,8 @@
-import { Text, View, Image } from 'react-native';
 import styled from 'styled-components/native';
 import { useState, useEffect } from 'react';
 
 
-export const VisualPicker = ({options}) => {
+export const VisualPicker = ({options, onChange}) => {
    const [activeOption, setActiveOption] = useState('');
 
    useEffect(()=>{
@@ -11,12 +10,13 @@ export const VisualPicker = ({options}) => {
    },[])
 
    const changeHandler = (value) => {
-      setActiveOption(value)
+      setActiveOption(value);
+      onChange(value);
    }
 
    return(
       <Container>
-         { options.map(({text, icon, value}) => <Option changeHandler={changeHandler} value={value} activeOption={activeOption} text={text} icon={icon}/>)}
+         { options.map(({text, icon, value}) => <Option key={value} changeHandler={changeHandler} value={value} activeOption={activeOption} text={text} icon={icon}/>)}
       </Container>
    )
 }
@@ -28,12 +28,19 @@ const Option = ({text, icon, activeOption, value, changeHandler}) => {
       <OptionContainer onTouchEnd={()=>changeHandler(value)} isActive={isActive}>
          <StyledImage source={icon}/>
          <StyledText>{text}</StyledText>
+         {isActive ?  
+            <ActiveMarker style={{transform: [{ rotate: "45deg" }]}}>
+               <StyledICon style={{transform: [{ rotate: "-45deg" }]}} source={require('../icons/check.png')}/>
+            </ActiveMarker>
+            : null
+         }
       </OptionContainer>
    )
 }
 
 
 const OptionContainer = styled.View` 
+   position: relative;
    width: 150px;
    height: 150px;
    background-color: #FFFFFF;
@@ -43,6 +50,7 @@ const OptionContainer = styled.View`
    justify-content: center;
    align-items: center;
    margin: 0px 5px 10px;
+   overflow: hidden;
 `
 const Container = styled.View` 
    display: flex;
@@ -50,7 +58,18 @@ const Container = styled.View`
    flex-wrap: wrap;
    justify-content: center;
 `
-
+const ActiveMarker = styled.View` 
+   width: 80px;
+   height: 80px;
+   display: flex;
+   align-items: center;
+   justify-content:flex-end;
+   background-color: rgb(248, 117, 45);
+   position: absolute;
+   top: -40px;
+   right: -40px;
+   padding: 5px;
+`
 const StyledText = styled.Text` 
     font-size: 18px;
     font-weight: 400;
@@ -60,4 +79,8 @@ const StyledImage = styled.Image`
    width: 60px;
    height: 60px;
    margin-bottom: 10px;
+`
+const StyledICon = styled.Image` 
+   width: 18px;
+   height: 18px;
 `
--- booking/pages/ChooseAccountType.js
+++ booking/pages/ChooseAccountType.js
@@ -1,7 +1,6 @@
-import { Text, View, Image } from 'react-native';
 import styled from 'styled-components/native'
 import { VisualPicker } from '../components/VisualPicker';
-import { Foundation } from '@expo/vector-icons'; 
+import { useState, useEffect } from 'react';
 
 
 const options = [
@@ -18,13 +17,41 @@ const options = [
 ]
 
 export const ChooseAccountType = () => {
+   const [accountType, setAccountType] = useState('customer');
+   const [hint, setHint] = useState('You will book  positions, or servi')
+
+   const changeHandler = (value) => {
+      setAccountType(value);
+   }
+
+   useEffect(()=>{
+      switch(accountType){
+         case 'business':
+            setHint('You will rent the desired positions, or provide services')
+            break
+         case 'customer':
+            setHint('You will book  positions, or services')
+            break
+         default:
+            setHint('')
+            break
+      }
+   },[accountType])
+   
+
    return (
       <Container>
          <TitleBox>
             <Title>Choose account type</Title>
+            <Hint>{hint}</Hint>
          </TitleBox>
          <ContentBox>
-           <VisualPicker options={options}/>
+           <VisualPicker onChange={changeHandler} options={options}/>
+           <ButtonBox>
+               <Button onTouchEnd={()=>console.log(accountType)}>
+                  <ButtonText>Next →</ButtonText>
+               </Button>
+           </ButtonBox>
          </ContentBox>
       </Container>
    )
@@ -35,19 +62,41 @@ const Container = styled.View`
    width: 100%;
 `
 const ContentBox = styled.View`
-   flex: 2;
-   justify-content: center;
+   flex: 1;
+   justify-content: space-between;
    align-items: center;
+   padding-bottom: 10%;
 `
 const TitleBox = styled.View`
+   padding-top: 20% ;
    flex: 1;
-   justify-content: center;
    align-items: center;
 `
-
+const Hint = styled.Text` 
+   font-size: 20px;
+   font-weight: 500;
+   color: #6c757d;
+   text-align: center;
+   margin-top: 50px;
+`
 const Title = styled.Text`
    font-size: 37px;
    font-weight: 600;
    text-align: center;
    color: #e76f51;
+`
+const Button = styled.View` 
+   width: 120px;
+   height: 40px;
+`
+const ButtonText = styled.Text` 
+   font-size: 30px;
+   color: rgb(248, 117, 45);
+`
+
+const ButtonBox = styled.View` 
+   display: flex;
+   flex-direction: row;
+   justify-content: flex-end;
+   width: 100%;
 `
